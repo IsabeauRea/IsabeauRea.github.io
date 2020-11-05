396175183102f45f96155461136d5df753783663
Isabeau Rea <isabeaurea@gmail.com>
2020-11-05 15:29:10 -0500

    d

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,1 +1,29 @@
 
+function displayMatches(e, dataSet) {
+    console.log(e.target.value);
+    const matches = matchWord(e.target.value, dataSet);
+    let placesHTML = matches.map(
+        (place) => `
+        <li>
+            <span class="name">${place.name}</span><br>
+            <span class="category">${place.category}</span>
+            <address>${place.address_line_1}<br>
+            ${place.city}<br>
+            ${place.zip}<address>
+            </li>
+            `);
+        if (e.target.value.length == 0) {
+            placesHTML = [];
+        }
+        return placesHTML;
+    }
+
+    async function SOMETHING() {
+        const data
+        const json
+        const input
+        input.addEvent
+            const
+            const
+            target.
+    }
