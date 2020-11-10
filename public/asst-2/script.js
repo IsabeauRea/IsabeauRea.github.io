55477f060b56fc17e908b16a64de8b070b0f33c9
Isabeau Rea <isabeaurea@gmail.com>
2020-11-09 19:19:00 -0500

    d

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -14,29 +14,27 @@ function findMatches(wordsToMatch, restaurants) {
 
 function displayMatches() {
     const matchArray = findMatches(this.value, restaurants);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const title = place.name.replace(regex, `<span class="h1">${this.value}</span>`);
-        const zipcode = place.zip.replace(regex, `<span class="h1">${this.value}</span>`);
-        const city = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
-        const category = place.category.replace(regex, `<span class="h1">${this.value}</span>`);
-        return `
-        <li>
-            <span class="name">${title}</span><br>
-            <span class="category">${place.category}</span>
-            <address>${place.address_line_1}<br>
-            ${place.city}<br>
-            ${place.zip}<address>
-            </li>
-
+    let placesHTML = []
+    if (this.value.length == 0) { 
+        placesHTML = [];
+    } 
+    else {placesHTML = matchArray.map((place) => `
+        
+    <li>
+        <span class="name">${place.name}</span><br>
+        <span class="category">${place.category}</span>
+        <address>${place.address_line_1}<br>
+        ${place.city}<br>
+        ${place.zip}<address>
         </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
+
+    </li>`).join('');
     }
+    suggestions.innerHTML = placesHTML;       
+}
+    
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('input', displayMatches);
